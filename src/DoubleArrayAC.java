a6b6b55b79d91d6eab5538834f2eafc14f0d5ea8
1170301027 <1354922069@qq.com>
2020-11-06 13:49:15 +0800

    Double Array Trie completion

--- src/DoubleArrayAC.java
+++ src/DoubleArrayAC.java
@@ -1,3 +1,5 @@
+import javafx.geometry.Pos;
+
 import java.util.*;
 
 /**
@@ -8,23 +10,23 @@ import java.util.*;
  */
 public class DoubleArrayAC {
 
-    private byte[] next;
+    //Next表，下标是位置偏移量，输出状态值
+    private Map<Integer, Integer> next = new HashMap<>();
     //Base表，下标是状态值，输出是base值，要求:当前状态的Base值+ASCII输入=下一个状态的偏移
-    private byte[] base;
+    private Map<Integer, Integer> base = new HashMap<>();
     //Check表，当前状态的父状态信息（唯一）
-    private byte[] check;
+    private Map<Integer, Integer> check = new HashMap<>();
+
+    private int curPos = 0;
 
     private State originState;
-    private List<State> allStates = new ArrayList<>();
+    private List<State> allStates = new ArrayList<>(); //下标即id
 
 
     public DoubleArrayAC(String[] pattern) {
         originState = new State(0);
         allStates.add(originState);
-
         constructAutomata(pattern);
-
-
     }
 
 
@@ -38,6 +40,32 @@ public class DoubleArrayAC {
         }
         originState.setFailure(originState);
         failure();
+
+        // 建表
+        base.put(0,-98);
+        next.put(0,0);
+        check.put(0,0);
+        for (int i = 0; i < allStates.size(); i++) {
+            State state = allStates.get(i);
+            for (State child : state.getChildren()) { //check表构建
+                if (!check.containsKey(child.getState())) {
+                    check.put(child.getState(),i);
+                }
+            }
+            List<Integer> list = new ArrayList<>();
+            List<Integer> states = new ArrayList<>();
+            List<Integer> bytes = new ArrayList<>();
+            for (Byte b : state.getTrans().keySet()) {
+                int offest = base.get(state.getState()) + b + getOffset(state.getState());
+                list.add(offest);
+                states.add(state.getTrans().get(b).getState());
+                bytes.add(Integer.valueOf(b));
+            }
+            for (int i1 = 0; i1 < list.size(); i1++) {
+                next.put(list.get(i1),states.get(i1));
+                base.put(states.get(i1), (list.get(i1) - curPos + base.get(state.getState())));
+            }
+        }
     }
 
     /**
@@ -55,7 +83,7 @@ public class DoubleArrayAC {
             i++;
         }
         for (int p = i; p < key.length; p++){
-            System.out.println(key[p] + "  "+ state.getState());
+            System.out.println(new String(new byte[] {key[p]}) + "  "+ state.getState());
             State newState = new State(allStates.size());
             state.addGoto(key[p], newState);
             state = newState;
@@ -79,6 +107,34 @@ public class DoubleArrayAC {
             return originState;
         }
         return null;
+    }
+
+    private State _goto_2(State state, byte input){
+        assert getOffset(state.getState()) == -1;
+//        System.out.println(getOffset(state.getState()));
+//        System.out.println(getOffset(state.getState()) + base.get(state.getState()) + input);
+//        System.out.println("input : " + input);
+        int offest = getOffset(state.getState()) + base.get(state.getState()) + input;
+        if (!next.containsKey(offest)) {
+            offest = 0;
+        }
+        int t = next.get(offest);
+        if (check.get(t) == state.getState()){
+//            System.out.println("curState :" + t);
+            return allStates.get(t);
+        } else {
+            System.out.println("Fail");
+            return null;
+        }
+    }
+
+    private Integer getOffset(int state) {
+        for (Integer integer : next.keySet()) {
+            if (state == next.get(integer)) {
+                return integer;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -110,6 +166,7 @@ public class DoubleArrayAC {
         }
     }
 
+
     public void run(String input){
         byte[] target = input.getBytes();
         State state = originState;
@@ -118,6 +175,27 @@ public class DoubleArrayAC {
                 state = state.getFailure();
             }
             state = _goto(state,b);
+            assert state != null;
+            if (state.isTerminalState()) {
+                System.out.println("State : " + state.getState());
+                System.out.print("Output : { ");
+                for (String s : state.getOutput()) {
+                    System.out.print(s + ",");
+                }
+                System.out.println("}");
+            }
+        }
+    }
+
+    public void run_2(String input){
+        byte[] target = input.getBytes();
+        State state = originState;
+        for (byte b : target) {
+            while (_goto_2(state,b) == null) {
+                state = state.getFailure();
+            }
+            state = _goto_2(state,b);
+//            System.out.println("run_2 state: " + state.getState());
             assert state != null;
             if (state.isTerminalState()) {
                 System.out.println("State : " + state.getState());
@@ -149,11 +227,32 @@ public class DoubleArrayAC {
         }
     }
 
+    public void showAutomata_2(){
+        System.out.println("Next : ");
+        System.out.print("[");
+        for (Integer integer : next.keySet()) {
+            System.out.print(integer + " " + next.get(integer) + "; ");
+        }
+        System.out.println("]");
+        System.out.println("Base : ");
+        System.out.print("[");
+        for (Integer integer : base.keySet()) {
+            System.out.print(integer + " " + base.get(integer) + "; ");
+        }
+        System.out.println("]");
+        System.out.println("Check : ");
+        System.out.print("[");
+        for (Integer integer : check.keySet()) {
+            System.out.print(integer + " " + check.get(integer) + "; ");
+        }
+        System.out.println("]");
+    }
+
     public static void main(String[] args) {
         String[] pattern = {"she","he","his"};
         DoubleArrayAC daac = new DoubleArrayAC(pattern);
-        daac.showAutomata();
-        daac.run("he his shehe");
+        daac.showAutomata_2();
+        daac.run_2("he his shehe");
     }
 
 }
